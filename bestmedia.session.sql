f4d24f52afc77c6da6632b2ef36917932425cbec
Tumusiime-Julius <juliustumusiime595@gmail.com>
2026-06-05 19:29:49 +0300

    Create Reviews Table

--- bestmedia.session.sql
+++ bestmedia.session.sql
@@ -78,3 +78,19 @@ CREATE TABLE payments (
         REFERENCES bookings(booking_id)
         ON DELETE CASCADE
 );
+//Create Reviews Table
+CREATE TABLE reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+
+    booking_id INT NOT NULL,
+
+    rating INT CHECK(rating BETWEEN 1 AND 5),
+
+    comment TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (booking_id)
+        REFERENCES bookings(booking_id)
+        ON DELETE CASCADE
+);
