2677e214cbf8445576d9503d69db036a5de6ff2f
Tumusiime-Julius <juliustumusiime595@gmail.com>
2026-06-06 12:24:46 +0300

    adding app.js, .env, and updating package.json

---
+++ booking-system-backend/app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Booking API Running");
+});
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
